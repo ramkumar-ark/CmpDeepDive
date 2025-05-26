83717f935900eb17942aeb73e255513809173374
Ram Kumar <ramgambit@gmail.com>
2025-05-26 17:06:41 +0530

    created server-status component

---
+++ src/app/server-status/server-status.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  imports: [],
+  templateUrl: './server-status.component.html',
+  styleUrl: './server-status.component.css',
+})
+export class ServerStatusComponent {
+  currentStatus = input<string>('online');
+}
