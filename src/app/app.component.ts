3ca120d13756ce39b32d5b69164a8db9bc3f6365
Ram Kumar <ramgambit@gmail.com>
2025-05-26 17:31:41 +0530

    created dashboard-item component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficComponent } from './traffic/traffic.component';
+import { DashboardItemComponent } from './dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    DashboardItemComponent,
+  ],
 })
 export class AppComponent {
   currentStatus = 'online';
