591ef9f71dcff870297c60a0df9b516e3c1cd588
Ram Kumar <ramgambit@gmail.com>
2025-05-27 12:04:22 +0530

    created new-ticket component

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../../../shared/button/button.component';
+
+@Component({
+  selector: 'app-new-ticket',
+  imports: [FormsModule, ButtonComponent],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent {}
