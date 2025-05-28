2b5b8988802cc810e5e6f7fc55fe44eff8e150e2
Ram Kumar <ramgambit@gmail.com>
2025-05-28 22:01:20 +0530

    created ticket component

---
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { Ticket } from './ticket.model';
+
+@Component({
+  selector: 'app-ticket',
+  imports: [],
+  templateUrl: './ticket.component.html',
+  styleUrl: './ticket.component.css',
+})
+export class TicketComponent {
+  ticketData = input.required<Ticket>();
+}
